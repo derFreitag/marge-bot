derFreitag/marge-bot

commit 88e7f7b6e0eccd695b6be927f9f146e97b6bf69a

Include more details when logging GitLabRebaseResultMismatch

marge/single_merge_job.py
@@ -57,8 +57,8 @@ class SingleMergeJob(MergeJob):
                     merge_request,
                     source_repo_url=source_repo_url,
                 )
-            except GitLabRebaseResultMismatch:
-                log.info("Gitlab rebase didn't give expected result")
+            except GitLabRebaseResultMismatch as err:
+                log.info("Gitlab rebase didn't give expected result: " + err.reason)
                 merge_request.comment("Someone skipped the queue! Will have to try again...")
                 continue
 

commit 01d5d7c6954b4604b68ebc7632a9c29b9b95d4a2

marge/single_merge_job: fix W1201(logging-not-lazy)
Fixes: 88e7f7b6e0ec ("Include more details when logging GitLabRebaseResultMismatch")

Signed-off-by: David Heidelberg <david@ixit.cz>

marge/single_merge_job.py
@@ -58,7 +58,7 @@ class SingleMergeJob(MergeJob):
                     source_repo_url=source_repo_url,
                 )
             except GitLabRebaseResultMismatch as err:
-                log.info("Gitlab rebase didn't give expected result: " + err.reason)
+                log.info("Gitlab rebase didn't give expected result: %s", err.reason)
                 merge_request.comment("Someone skipped the queue! Will have to try again...")
                 continue
 