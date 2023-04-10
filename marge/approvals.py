bc37a43b4c90e409813955c5fcc2355991093169
John L. Villalovos <john@sodarock.com>
2023-04-09 18:48:16 -0700

    chore: add type-hints to `marge/approvals.py`

--- marge/approvals.py
+++ marge/approvals.py
@@ -1,12 +1,13 @@
-from . import gitlab
+from typing import TYPE_CHECKING, List, cast
 
-GET, POST, PUT = gitlab.GET, gitlab.POST, gitlab.PUT
+from . import gitlab
 
 
 class Approvals(gitlab.Resource):
-    """Approval info for a MergeRequest."""
+    """Approval info for a MergeRequest.
+    https://docs.gitlab.com/ee/api/merge_request_approvals.html"""
 
-    def refetch_info(self):
+    def refetch_info(self) -> None:
         gitlab_version = self._api.version()
         if gitlab_version.release >= (9, 2, 2):
             approver_url = (
@@ -20,36 +21,39 @@ class Approvals(gitlab.Resource):
 
         # Approvals are in CE since 13.2
         if gitlab_version.is_ee or gitlab_version.release >= (13, 2, 0):
-            self._info = self._api.call(GET(approver_url))
+            info = self._api.call(gitlab.GET(approver_url))
         else:
-            self._info = dict(self._info, approvals_left=0, approved_by=[])
+            info = dict(self._info, approvals_left=0, approved_by=[])
+        if TYPE_CHECKING:
+            assert isinstance(info, dict)
+        self._info = info
 
     @property
-    def iid(self):
-        return self.info["iid"]
+    def iid(self) -> int:
+        return cast(int, self.info["iid"])
 
     @property
-    def project_id(self):
-        return self.info["project_id"]
+    def project_id(self) -> int:
+        return cast(int, self.info["project_id"])
 
     @property
-    def approvals_left(self):
+    def approvals_left(self) -> int:
         return self.info.get("approvals_left", 0)
 
     @property
-    def sufficient(self):
+    def sufficient(self) -> bool:
         return not self.approvals_left
 
     @property
-    def approver_usernames(self):
+    def approver_usernames(self) -> List[str]:
         return [who["user"]["username"] for who in self.info["approved_by"]]
 
     @property
-    def approver_ids(self):
+    def approver_ids(self) -> List[int]:
         """Return the uids of the approvers."""
         return [who["user"]["id"] for who in self.info["approved_by"]]
 
-    def reapprove(self):
+    def reapprove(self) -> None:
         """Impersonates the approvers and re-approves the merge_request as them.
 
         The idea is that we want to get the approvers, push the rebased branch
@@ -58,7 +62,7 @@ class Approvals(gitlab.Resource):
         """
         self.approve(self)
 
-    def approve(self, obj):
+    def approve(self, obj: "Approvals") -> None:
         """Approve an object which can be a merge_request or an approval."""
         if self._api.version().release >= (9, 2, 2):
             approve_url = f"/projects/{obj.project_id}/merge_requests/{obj.iid}/approve"
@@ -67,4 +71,4 @@ class Approvals(gitlab.Resource):
             approve_url = f"/projects/{obj.project_id}/merge_requests/{obj.id}/approve"
 
         for uid in self.approver_ids:
-            self._api.call(POST(approve_url), sudo=uid)
+            self._api.call(gitlab.POST(approve_url), sudo=uid)
