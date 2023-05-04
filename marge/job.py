derFreitag/marge-bot

commit d5a57ec9b5f93fe0a772cd50479af8b38b40424e

marge: fix C0209(consider-using-f-string)
Signed-off-by: David Heidelberg <david@ixit.cz>

marge/job.py
@@ -56,7 +56,7 @@ class MergeJob:
         if not approvals.sufficient:
             raise CannotMerge(
                 'Insufficient approvals '
-                '(have: {0.approver_usernames} missing: {0.approvals_left})'.format(approvals)
+                '(have: {approvals.approver_usernames} missing: {approvals.approvals_left})'
             )
 
         if not merge_request.blocking_discussions_resolved:
@@ -65,8 +65,8 @@ class MergeJob:
         state = merge_request.state
         if state not in ('opened', 'reopened', 'locked'):
             if state in ('merged', 'closed'):
-                raise SkipMerge('The merge request is already {}!'.format(state))
-            raise CannotMerge('The merge request is in an unknown state: {}'.format(state))
+                raise SkipMerge(f'The merge request is already {state}!')
+            raise CannotMerge(f'The merge request is in an unknown state: {state}')
 
         if self.during_merge_embargo():
             raise SkipMerge('Merge embargo!')
@@ -108,7 +108,7 @@ class MergeJob:
         )
 
         tested_by = (
-            ['{0._user.name} <{1.web_url}>'.format(self, merge_request)]
+            [f'{self._user.name} <{merge_request.web_url}>']
             if should_add_tested
             else None
         )
@@ -126,7 +126,7 @@ class MergeJob:
             self._options.fusion is not Fusion.gitlab_rebase
         )
         part_of = (
-            '<{0.web_url}>'.format(merge_request)
+            '<f{merge_request.web_url}>'
             if should_add_parts_of
             else None
         )
@@ -334,7 +334,7 @@ class MergeJob:
             # the sha from the remote target branch.
             target_sha = repo.get_commit_hash('origin/' + target_branch)
             if updated_sha == target_sha:
-                raise CannotMerge('these changes already exist in branch `{}`'.format(target_branch))
+                raise CannotMerge(f'these changes already exist in branch `{target_branch}`')
             final_sha = self.add_trailers(merge_request) if add_trailers else None
             final_sha = final_sha or updated_sha
             commits_rewrite_done = True
@@ -403,14 +403,14 @@ class MergeJob:
                 raise CannotMerge("Sorry, I can't modify protected branches!") from err
 
             change_type = "merged" if self.opts.fusion == Fusion.merge else "rebased"
-            raise CannotMerge('Failed to push %s changes, check my logs!' % change_type) from err
+            raise CannotMerge(f'Failed to push {change_type} changes, check my logs!') from err
 
     def synchronize_using_gitlab_rebase(self, merge_request, expected_sha=None):
         expected_sha = expected_sha or self._repo.get_commit_hash()
         try:
             merge_request.rebase()
         except MergeRequestRebaseFailed as err:
-            raise CannotMerge("GitLab failed to rebase the branch saying: {0[0]}".format(err.args)) from err
+            raise CannotMerge(f"GitLab failed to rebase the branch saying: {err.args[0]}") from err
         except TimeoutError as err:
             raise CannotMerge("GitLab was taking too long to rebase the branch...") from err
         except gitlab.ApiError as err:
@@ -437,7 +437,7 @@ def _get_reviewer_names_and_emails(commits, approvals, api):
     self_reviewed = {commit['author_email'] for commit in commits} & {user.email for user in users}
     if self_reviewed and len(users) <= 1:
         raise CannotMerge('Commits require at least one independent reviewer.')
-    return ['{0.name} <{0.email}>'.format(user) for user in users]
+    return [f'{user.name} <{user.email}>' for user in users]
 
 
 # pylint: disable=invalid-name
@@ -516,5 +516,5 @@ class GitLabRebaseResultMismatch(CannotMerge):
     def __init__(self, gitlab_sha, expected_sha):
         super().__init__(
             "GitLab rebase ended up with a different commit:"
-            "I expected %s but they got %s" % (expected_sha, gitlab_sha)
+            f"I expected {expected_sha} but they got {gitlab_sha}"
         )

marge/app.py
@@ -28,7 +28,7 @@ def time_interval(str_interval):
         return timedelta(**{translate[unit or 's']: float(quant)})
     except (AttributeError, ValueError) as err:
         raise configargparse.ArgumentTypeError(
-                'Invalid time interval (e.g. 12[s|min|h]): %s' % str_interval
+                f'Invalid time interval (e.g. 12[s|min|h]): {str_interval}'
         ) from err
 
 
@@ -38,7 +38,7 @@ def _parse_config(args):  # pylint: disable=too-many-statements
         try:
             return re.compile(str_regex)
         except re.error as err:
-            raise configargparse.ArgumentTypeError('Invalid regexp: %r (%s)' % (str_regex, err.msg))
+            raise configargparse.ArgumentTypeError(f'Invalid regexp: {str_regex!r} ({err.msg})')
 
     parser = configargparse.ArgParser(
         auto_env_var_prefix='MARGE_',
@@ -258,7 +258,7 @@ def _parse_config(args):  # pylint: disable=too-many-statements
         ]
         for flag in conflicting_flag:
             if getattr(config, flag[2:].replace("-", "_")):
-                raise MargeBotCliArgError('--rebase-remotely and %s are mutually exclusive' % flag)
+                raise MargeBotCliArgError(f'--rebase-remotely and {flag} are mutually exclusive')
 
     cli_args = []
     # pylint: disable=protected-access
@@ -266,7 +266,7 @@ def _parse_config(args):  # pylint: disable=too-many-statements
         cli_args.extend(value)
     for bad_arg in ['--auth-token', '--ssh-key']:
         if any(bad_arg in arg for arg in cli_args):
-            raise MargeBotCliArgError('"%s" can only be set via ENV var or config file.' % bad_arg)
+            raise MargeBotCliArgError(f'"{bad_arg}" can only be set via ENV var or config file.')
     return config
 
 
@@ -319,7 +319,7 @@ def main(args=None):
             if version.release < (11, 6):
                 raise Exception(
                     "Need GitLab 11.6+ to use rebase through the API, "
-                    "but your instance is {}".format(version)
+                    f"but your instance is {format(version)}"
                 )
             fusion = bot.Fusion.gitlab_rebase
         else:

marge/approvals.py
@@ -9,10 +9,10 @@ class Approvals(gitlab.Resource):
     def refetch_info(self):
         gitlab_version = self._api.version()
         if gitlab_version.release >= (9, 2, 2):
-            approver_url = '/projects/{0.project_id}/merge_requests/{0.iid}/approvals'.format(self)
+            approver_url = f'/projects/{self.project_id}/merge_requests/{self.iid}/approvals'
         else:
             # GitLab botched the v4 api before 9.2.3
-            approver_url = '/projects/{0.project_id}/merge_requests/{0.id}/approvals'.format(self)
+            approver_url = f'/projects/{self.project_id}/merge_requests/{self.id}/approvals'
 
         # Approvals are in CE since 13.2
         if gitlab_version.is_ee or gitlab_version.release >= (13, 2, 0):
@@ -57,10 +57,10 @@ class Approvals(gitlab.Resource):
     def approve(self, obj):
         """Approve an object which can be a merge_request or an approval."""
         if self._api.version().release >= (9, 2, 2):
-            approve_url = '/projects/{0.project_id}/merge_requests/{0.iid}/approve'.format(obj)
+            approve_url = f'/projects/{obj.project_id}/merge_requests/{obj.iid}/approve'
         else:
             # GitLab botched the v4 api before 9.2.3
-            approve_url = '/projects/{0.project_id}/merge_requests/{0.id}/approve'.format(obj)
+            approve_url = f'/projects/{obj.project_id}/merge_requests/{obj.id}/approve'
 
         for uid in self.approver_ids:
             self._api.call(POST(approve_url), sudo=uid)

marge/batch_job.py
@@ -89,7 +89,7 @@ class BatchMergeJob(MergeJob):
             except CannotMerge as ex:
                 log.warning('Skipping unmergeable MR: "%s"', ex)
                 self.unassign_from_mr(merge_request)
-                merge_request.comment("I couldn't merge this branch: {}".format(ex))
+                merge_request.comment(f"I couldn't merge this branch: {ex}")
             else:
                 mergeable_mrs.append(merge_request)
         return mergeable_mrs
@@ -215,10 +215,10 @@ class BatchMergeJob(MergeJob):
 
         # Save the sha of remote <target_branch> so we can use it to make sure
         # the remote wasn't changed while we're testing against it
-        remote_target_branch_sha = self._repo.get_commit_hash('origin/%s' % target_branch)
+        remote_target_branch_sha = self._repo.get_commit_hash(f'origin/{target_branch}')
 
-        self._repo.checkout_branch(target_branch, 'origin/%s' % target_branch)
-        self._repo.checkout_branch(BatchMergeJob.BATCH_BRANCH_NAME, 'origin/%s' % target_branch)
+        self._repo.checkout_branch(target_branch, f'origin/{target_branch}')
+        self._repo.checkout_branch(BatchMergeJob.BATCH_BRANCH_NAME, f'origin/{target_branch}')
 
         batch_mr = self.create_batch_mr(
             target_branch=target_branch,
@@ -232,7 +232,7 @@ class BatchMergeJob(MergeJob):
                 _, source_repo_url, merge_request_remote = self.fetch_source_project(merge_request)
                 self._repo.checkout_branch(
                     merge_request.source_branch,
-                    '%s/%s' % (merge_request_remote, merge_request.source_branch),
+                    f'{merge_request_remote}/{merge_request.source_branch}',
                 )
 
                 if self._options.use_merge_commit_batches:
@@ -246,11 +246,8 @@ class BatchMergeJob(MergeJob):
                         BatchMergeJob.BATCH_BRANCH_NAME,
                         merge_request.source_branch,
                         '-m',
-                        'Batch merge !%s into %s (!%s)' % (
-                            merge_request.iid,
-                            merge_request.target_branch,
-                            batch_mr.iid
-                        ),
+                        f'Batch merge !{merge_request.iid} into' +
+                        f'{merge_request.target_branch} (!{batch_mr.iid})',
                         local=True,
                     )
                 else:
@@ -288,7 +285,7 @@ class BatchMergeJob(MergeJob):
         # This switches git to <batch> branch
         self.push_batch()
         for merge_request in working_merge_requests:
-            merge_request.comment('I will attempt to batch this MR (!{})...'.format(batch_mr.iid))
+            merge_request.comment(f'I will attempt to batch this MR (!{batch_mr.iid})...')
 
         # wait for the CI of the batch MR
         if self._project.only_allow_merge_if_pipeline_succeeds:
@@ -297,10 +294,7 @@ class BatchMergeJob(MergeJob):
             except CannotMerge as err:
                 for merge_request in working_merge_requests:
                     merge_request.comment(
-                        'Batch MR !{batch_mr_iid} failed: {error} I will retry later...'.format(
-                            batch_mr_iid=batch_mr.iid,
-                            error=err.reason,
-                        ),
+                        f'Batch MR !{batch_mr.iid} failed: {err.reason} I will retry later...',
                     )
                 raise CannotBatch(err.reason) from err
 
@@ -322,9 +316,7 @@ class BatchMergeJob(MergeJob):
                     )
             except CannotBatch as err:
                 merge_request.comment(
-                    "I couldn't merge this branch: {error} I will retry later...".format(
-                        error=str(err),
-                    ),
+                    f"I couldn't merge this branch: {str(err)} I will retry later...",
                 )
                 raise
             except SkipMerge:
@@ -332,7 +324,7 @@ class BatchMergeJob(MergeJob):
                 raise
             except CannotMerge as err:
                 self.unassign_from_mr(merge_request)
-                merge_request.comment("I couldn't merge this branch: %s" % err.reason)
+                merge_request.comment(f"I couldn't merge this branch: {err.reason}")
                 raise
 
         # Accept the batch MR
@@ -354,4 +346,4 @@ class BatchMergeJob(MergeJob):
                 log.info('batch_mr.accept result: %s', ret)
             except gitlab.ApiError as err:
                 log.exception('Gitlab API Error:')
-                raise CannotMerge('Gitlab API Error: %s' % err) from err
+                raise CannotMerge(f'Gitlab API Error: {err}') from err

marge/bot.py
@@ -24,10 +24,10 @@ class Bot:
 
         if not user.is_admin:
             assert not opts.reapprove, (
-                "{0.username} is not an admin, can't impersonate!".format(user)
+                f"{user.username} is not an admin, can't impersonate!"
             )
             assert not opts.add_reviewers, (
-                "{0.username} is not an admin, can't lookup Reviewed-by: email addresses ".format(user)
+                f"{user.username} is not an admin, can't lookup Reviewed-by: email addresses "
             )
 
     def start(self):

marge/branch.py
@@ -9,10 +9,7 @@ class Branch(gitlab.Resource):
     @classmethod
     def fetch_by_name(cls, project_id, branch, api):
         info = api.call(GET(
-            '/projects/{project_id}/repository/branches/{branch}'.format(
-                project_id=project_id,
-                branch=branch,
-            ),
+            f'/projects/{project_id}/repository/branches/{branch}',
         ))
         return cls(api, info)
 

marge/commit.py
@@ -13,20 +13,14 @@ class Commit(gitlab.Resource):
     @classmethod
     def fetch_by_id(cls, project_id, sha, api):
         info = api.call(GET(
-            '/projects/{project_id}/repository/commits/{sha}'.format(
-                project_id=project_id,
-                sha=sha,
-            ),
+            f'/projects/{project_id}/repository/commits/{sha}',
         ))
         return cls(api, info)
 
     @classmethod
     def last_on_branch(cls, project_id, branch, api):
         info = api.call(GET(
-            '/projects/{project_id}/repository/branches/{branch}'.format(
-                project_id=project_id,
-                branch=quote(branch, safe=''),
-            ),
+            f'/projects/{project_id}/repository/branches/{quote(branch, safe="")}',
         ))['commit']
         return cls(api, info)
 

marge/git.py
@@ -19,14 +19,13 @@ GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no "
 
 
 def _filter_branch_script(trailer_name, trailer_values):
-    filter_script = 'TRAILERS={trailers} python3 {script}'.format(
-        trailers=shlex.quote(
+    trailers: str = shlex.quote(
             '\n'.join(
-                '{}: {}'.format(trailer_name, trailer_value)
+                f'{trailer_name}: {trailer_value}'
                 for trailer_value in trailer_values or [''])
-        ),
-        script=trailerfilter.__file__,
-    )
+            )
+
+    filter_script = f'TRAILERS={trailers} python3 {trailerfilter.__file__}'
     return filter_script
 
 
@@ -148,7 +147,7 @@ class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file timeout refere
             source = 'origin'
         force_flag = '--force' if force else ''
         skip_flag = ('-o', 'ci.skip') if skip_ci else ()
-        self.git('push', force_flag, *skip_flag, source, '%s:%s' % (branch, branch))
+        self.git('push', force_flag, *skip_flag, source, f'{branch}:{branch}')
 
     def get_commit_hash(self, rev='HEAD'):
         """Return commit hash for `rev` (default "HEAD")."""
@@ -156,7 +155,7 @@ class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file timeout refere
         return result.stdout.decode('ascii').strip()
 
     def get_remote_url(self, name):
-        return self.git('config', '--get', 'remote.{}.url'.format(name)).stdout.decode('utf-8').strip()
+        return self.git('config', '--get', f'remote.{name}.url').stdout.decode('utf-8').strip()
 
     def git(self, *args, from_repo=True):
         env = None

commit 13fb9a11c48708763f60e0f30ac3aa9829225c63

job: fixes variable presentation
"Part-of: <f{merge_request.web_url}>"

Fixes: d5a57ec9b5f9 ("marge: fix C0209(consider-using-f-string)")

Signed-off-by: David Heidelberg <david@ixit.cz>

marge/job.py
@@ -138,7 +138,7 @@ class MergeJob:
             self._options.add_part_of
             and self._options.fusion is not Fusion.gitlab_rebase
         )
-        part_of = "<f{merge_request.web_url}>" if should_add_parts_of else None
+        part_of = f"<{merge_request.web_url}>" if should_add_parts_of else None
         if part_of is not None:
             sha = self._repo.tag_with_trailer(
                 trailer_name="Part-of",